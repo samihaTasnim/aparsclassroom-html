247eb91780b6071e13448fe33756e8ca5c05214e
Samiha Tasnim <samxtasnim@gmail.com>
2023-09-12 13:05:43 +0600

    contact page ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './index.js'],
+  content: ['./index.html', './index.js', './contact.html', './privacy.html', './about-us.html'],
   theme: {
     extend: {},
   },
